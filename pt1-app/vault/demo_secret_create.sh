dc733aa8bb00a431389a02e18283cd18488b3e30
NeoStudyHat <85171551+Daniel-Giszpenc@users.noreply.github.com>
2026-07-16 03:29:39 -0400

    update bash script with token and lease commands

--- pt1-app/vault/demo_secret_create.sh
+++ pt1-app/vault/demo_secret_create.sh
@@ -9,3 +9,12 @@ vault kv put infra-secrets/tailscale_aws_auth_key \
     msg="<redacted>"
 
 vault kv put infra-secrets/tailscale_aws_auth_key key="<redacted-v2>"
+
+vault token create -ttl=1h -use-limit=2 -policy="developer-access"
+export USE_LIMIT_TOKEN=<your-token>
+VAULT_TOKEN=$USE_LIMIT_TOKEN vault token lookup
+
+vault list sys/leases/lookup
+vault list sys/leases/lookup/aws/creds/vault-terraform
+vault lease lookup aws/creds/vault-terraform/short-id
+vault lease revoke aws/creds/vault-terraform/short-id
